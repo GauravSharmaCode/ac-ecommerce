6e6fd559757d705e6578c674cf0dc3737bed6ba1
GauravSharmaCode <shrma.gurv@gmail.com>
2025-06-16 00:36:00 +0530

    addeed portolio

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Portfolio(models.Model):
+    name = models.CharField(max_length=100)
+    website = models.URLField(blank=True)
+    logo = models.ImageField(upload_to='clients_logos/')
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        """
+        Returns the client's name as a string representation of the object.
+        """
+        
+        return self.name
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.generics import ListAPIView
+from .models import Portfolio
+from .serializers import PortfolioSerializer
+
+class PortfolioListView(ListAPIView):
+    serializer_class = PortfolioSerializer
+
+    def get_queryset(self):
+             """
+    Return a queryset of active Portfolio objects.
+
+    This method filters the Portfolio objects to include only those
+    that are marked as active.
+
+    Returns:
+        QuerySet: A QuerySet containing active Portfolio objects.
+    """
+            ###############################################################################
+            # This method is used to filter the Portfolio objects to include only those
+            # that are marked as active.
+            ###############################################################################
+             return Portfolio.objects.filter(is_active=True)
+            
